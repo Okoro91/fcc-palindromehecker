45ff97664c7db65f8a67e89525b8b4b6e5c40a80
okoro91 <mi.fracri91@gmail.com>
2025-05-22 10:22:30 +0100

    added regex and improve code functionality

--- app.js
+++ app.js
@@ -4,31 +4,29 @@ const displayResult = document.querySelector("#result");
 
 
 
-
 const reverseWord = (userInput) => {
-   const reverseText = userInput.split('').reverse().join('');
-   if(reverseText === userInput){
-    return `${reverseText} is a palindrome`;
+   const textClean = userInput.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+   const reverseText = textClean.split('').reverse().join('');
+   if(reverseText === textClean){
+    return `${userInput} is a palindrome`;
    }else{
     return `${userInput} is not a palindrome`;
    }
 }
 
- const checkTextInput = () => {
-    const text = inputText.value;
-    if(text === ''){
-        alert("Please input a value")
-    }
- }
+ 
 
 
 const displayUserInput = () => {
-    checkTextInput();
     const text = inputText.value;
-    const reverseText = reverseWord(text)
+    if(text === ''){
+       alert("Please input a value")
+    }
+    const reverseText = reverseWord(text);
     displayResult.style.display = "block";
     displayResult.innerText = reverseText;
     inputText.value = "";
+
 }
 
 
